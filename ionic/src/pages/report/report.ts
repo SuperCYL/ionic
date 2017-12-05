1b1f204a27944dea2fcb95554160f6071b2d26c0
SuperCYL <949330777@qq.com>
2017-12-05 16:43:59 +0800

    国际化处理

--- ionic/src/pages/report/report.ts
+++ ionic/src/pages/report/report.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import $ from 'jquery';
-import {TranslateHelper} from "../../providers/helper/TranslateHelper";
 
 /**
  * Generated class for the ReportPage page.
@@ -17,23 +16,18 @@ import {TranslateHelper} from "../../providers/helper/TranslateHelper";
 })
 export class ReportPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private translate:TranslateHelper) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReportPage');
     $("#reportTab li").click(function(){
       var isActive = $(this).hasClass('tab-active');
+      var text = $(this).text();
+      $(".pg-tit span").html(text);
       if(isActive === false){
           $(this).addClass('tab-active');
           $(this).siblings('li').removeClass('tab-active');
-      }
-      var text = $(this).text();
-      if(text == 'Last Week'){
-        $(".pg-tit").html("Heat Map in the Last Week")
-      }
-      if(text == 'Last Month'){
-        $(".pg-tit").html("Heat Map in the Last Month")
       }
     })
   }
